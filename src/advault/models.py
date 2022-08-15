df98a9f110714bae108c7ab396083928b728ab9b
Pooja-Lohar25 <poojalohar789@gmail.com>
2022-08-15 05:45:15 +0530

    db creation file

--- src/advault/models.py
+++ src/advault/models.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from datetime import datetime
+import datetime
 from sqlmodel import Field, SQLModel
 
 
@@ -9,8 +9,6 @@ class Ad_Hub(SQLModel,table=True):
     adhar_key: str = Field(primary_key=True)
     uid:str
 
-
-
 class DemoHub(SQLModel,table=True):
     id: Optional[int] = Field(default=None)
     demo_key:str  = Field(primary_key=True)
@@ -18,14 +16,13 @@ class DemoHub(SQLModel,table=True):
 
 class AdDm_Link(SQLModel,table=True):
     ad_demo_key:str = Field(primary_key=True)
-    adhar_key:str = Field(foreign_key = "Ad_Hub.adhar_key")
-    demo_key:str = Field(foreign_key = "DemoHub.demo_key")
-
+    adhar_key:str = Field(foreign_key = "ad_hub.adhar_key")
+    demo_key:str = Field(foreign_key = "demohub.demo_key")
 
 
 class Ad_Satellite(SQLModel,table=True):
     id:Optional[int] = Field(default=None,primary_key=True)
-    adhar_key:str = Field(foreign_key="Ad_Hub.adhar_key")
+    adhar_key:str = Field(foreign_key="ad_hub.adhar_key")
     name:str 
     dob:datetime.datetime
     gender:str
