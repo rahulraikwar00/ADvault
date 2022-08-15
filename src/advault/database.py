c23baf5541e390bad93ddd13995f18816a8c57aa
Pooja-Lohar25 <poojalohar789@gmail.com>
2022-08-15 06:02:19 +0530

    db creation file

--- src/advault/database.py
+++ src/advault/database.py
@@ -7,7 +7,6 @@ if not sqlalchemy_utils.functions.database_exists(url):
     sqlalchemy_utils.functions.create_database(url)
 engine = create_engine(url,echo=True)
 
-
 def cr_db():
     SQLModel.metadata.create_all(engine) #create all the tables in the database
 
