3941094a8956fe0ad58301d297b700f86c22485c
rahul raikwar <rr200636@gmail.com.com>
2022-08-17 16:09:47 +0530

    [FIXed] Fix - formate models

--- src/advault/db/models.py
+++ src/advault/db/models.py
@@ -3,16 +3,21 @@ import datetime
 from sqlmodel import Field, SQLModel
 
 
+class Aadhaar(SQLModel):
+    aadhaar_key: str = Field(foreign_key="ad_hub.aadhaar_key")
+    status: str
+    timestamp: datetime.datetime
+
+
 class Ad_Hub(SQLModel, table=True):
     id: Optional[int] = Field(default=None)
     aadhaar_key: str = Field(primary_key=True)
     uid: str
 
 
-class Poi_Sat(SQLModel, table=True):
-    id: Optional[int] = Field(default=None,primary_key=True)
-    aadhaar_key :str = Field(foreign_key="ad_hub.aadhaar_key")
-    careof: str
+class Poi_Sat(Aadhaar, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    careof: str 
     country: str
     dist: str
     house: str
@@ -25,9 +30,9 @@ class Poi_Sat(SQLModel, table=True):
     subdist: str
     vtc: str
 
-class Poa_Sat(SQLModel, table=True):
-    id: Optional[int] = Field(default=None,primary_key=True)
-    aadhaar_key :str = Field(foreign_key="ad_hub.aadhaar_key")
+
+class Poa_Sat(Aadhaar, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     dob: str
     e: Optional[str] = Field(default=None)
     gender: str
@@ -35,16 +40,14 @@ class Poa_Sat(SQLModel, table=True):
     name: str
 
 
-class Ad_Aud_Link(SQLModel, table=True):
+class Ad_Aud_Link(Aadhaar, table=True):
     ad_aud_key: str = Field(primary_key=True)
-    aadhaar_key: str = Field(foreign_key="ad_hub.aadhaar_key")
     audit_key: str = Field(foreign_key="audit_hub.audit_key")
 
 
-class AD_salt_sat(SQLModel, table=True):
+class AD_salt_sat(Aadhaar, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     salt: str = Field(primary_key=True)
-    aadhaar_key: str = Field(foreign_key="ad_hub.aadhaar_key")
 
 
 class Audit_Hub(SQLModel, table=True):
@@ -52,11 +55,10 @@ class Audit_Hub(SQLModel, table=True):
     timestamp: datetime.datetime = Field(default=datetime.datetime.now())
 
 
-# creater server audit tabl
-class Server_Audit_Sat(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    audit_key: str = Field(foreign_key="audit_hub.audit_key")
+# for monitoring purpose
+class Audit_Log(SQLModel):
     timestamp: datetime.datetime = Field(default=datetime.datetime.now())
+    audit_key: str = Field(foreign_key="audit_hub.audit_key")
     status: str = Field(default="pending")
     reason: str = Field(default="")
     action: str = Field(default="")
@@ -65,13 +67,11 @@ class Server_Audit_Sat(SQLModel, table=True):
     action_remarks: str = Field(default="")
 
 
-# create user audits table
-class User_Audit_Sat(SQLModel, table=True):
+# creater server audit tabl
+class Server_Audit_Sat(Audit_Log, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    audit_key: str = Field(foreign_key="audit_hub.audit_key")
-    timestamp: datetime.datetime = Field(default=datetime.datetime.now())
-    status: str = Field(default="pending")
-    reason: str = Field(default="")
-    action: str = Field(default="")
-    action_by: str = Field(default="")
-    action_on: datetime.datetime = Field(default=datetime.datetime.now())
+
+
+# create user audits table
+class User_Audit_Sat(Audit_Log, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
