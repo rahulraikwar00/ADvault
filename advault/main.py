a35be733c3341a9ee72f9fc16475e4dfdd985388
rahul raikwar <rr200636@gmail.com.com>
2022-08-19 04:04:37 +0530

    [ADD-FILEs] schema.py

--- advault/main.py
+++ advault/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from db.database import *
 from db.crud import *
 from db.models import *
+from db.schema import *
 
 app = FastAPI()
 
@@ -17,18 +18,7 @@ async def create():
 @app.post("/upload")
 async def upload(data: Aadhaar):
     ins_data_hub(data)
-
     return {"message": "Data uploaded"}
-
-
-@app.get("/download/{refrence_id}")
-async def download(refrence_id: str):
-    pass
-
-
-@app.get("/download/{refrence_id}/{file_name}")
-async def download(refrence_id: str, file_name: str):
-    pass
 
 
 @app.get("/download_all/")
