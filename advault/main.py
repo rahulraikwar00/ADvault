76b71e107a503a2e5fc348a0a1c9f7c1fab6774a
Pooja-Lohar25 <poojalohar789@gmail.com>
2022-08-23 20:51:36 +0530

    response body resolved

--- advault/main.py
+++ advault/main.py
@@ -7,7 +7,7 @@ from db.database import *
 from db.crud import *
 from db.models import *
 from db.schema import *
-
+from auth.user import *
 from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
 app = FastAPI()
 
@@ -40,19 +40,13 @@ async def download_by_premsat(multiSelctionDropdown: List[select_params] = Query
 
 #api authentication test
 
-from datetime import datetime, timedelta
-from typing import Union
+
 
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
 # to get a string like this run:
 # openssl rand -hex 32
-SECRET_KEY = "ba4bb170358d997b5b1b262a3263619be867e7ce687e252067e0404ffe5f5a22"
-ALGORITHM = "HS256"
-
-# expire
-ACCESS_TOKEN_EXPIRE_MINUTES = 1
 
 
 
@@ -63,26 +57,13 @@ def get_u():
     res = {}
     for i in users_db:
         res[i['username']] = i
-    fake_users_db = res
-    return fake_users_db
+    print("get data: ", res)
+    return res
 
     
 
 
-class Token(SQLModel):
-    access_token: str
-    token_type: str
 
-
-class TokenData(SQLModel):
-    username: Union[str, None] = None
-
-
-class User(SQLModel):
-    username: str
-    email: Union[str, None] = None
-    full_name: Union[str, None] = None
-    disabled: Union[bool, None] = None
 
 
 # class UserInDB(User_data):
@@ -91,78 +72,13 @@ class User(SQLModel):
 
 # pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return user_dict
-
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return user
-    return user
-
-
-def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    print("encoded_jwt", encoded_jwt)
-    return encoded_jwt
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    fake_users_db = get_u()
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(current_user: User_data = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
 
 @app.post("/token")
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     try:
         fake_users_db = get_u()
         user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+        # print(user)
         if not user:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
@@ -174,6 +90,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         access_token = create_access_token(
             data={"sub": user.username}, expires_delta=access_token_expires 
         )
+        print(access_token)
 
         return {"access_token": access_token, "token_type": "bearer"}
     except Exception as e:
