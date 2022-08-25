f3a8353bb3a118a0f8fd3bdcd9cfa5976ddd1ce4
Pooja-Lohar25 <poojalohar789@gmail.com>
2022-08-25 20:52:26 +0530

    basic arranging and formatting

--- advault/main.py
+++ advault/main.py
@@ -121,9 +121,9 @@ async def read_users_me(current_user: User_data = Depends(get_current_active_use
     return current_user
 
 
-@app.get("/users/me/items/")
-async def read_own_items(current_user: User_data = Depends(get_current_active_user)):
-    return [{"item_id": "Foo", "owner": current_user.username}]
+# @app.get("/users/me/items/")
+# async def read_own_items(current_user: User_data = Depends(get_current_active_user)):
+#     return [{"item_id": "Foo", "owner": current_user.username}]
 
 
 
